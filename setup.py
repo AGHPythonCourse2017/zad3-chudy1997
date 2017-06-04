59f72afea0dd188c62340189f1b6a67f2353a6aa
chudy1997 <karolbartyzel@interia.pl>
2017-06-04 08:19:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup
 
 setup(name='singer_song_truth',
       version='1.0',
-      description='Project allowing checking if sentence about posessness of song is true.',
+      description='Project allowing checking if '
+      '     sentence about posessness of song is true.',
       url='https://github.com/AGHPythonCourse2017/zad3-chudy1997',
       author='Karol Bartyzel',
       author_email='karolbartyzel308@gmail.com',
